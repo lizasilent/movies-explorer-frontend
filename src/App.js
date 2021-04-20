2f46aa766f3ffda1762d45ced9ba2b6b5cd35f90
lizasilent <elizavetad@yandex.ru>
2021-04-20 23:35:59 +0300

    added 2 more blocks

--- src/App.js
+++ src/App.js
@@ -17,38 +17,72 @@ function App() {
           <main>
             <div className="promo">
               <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
-            </div>
             <div className="navigation">
-              <nav>
                 <ul className="navigation__menu">
               <li><a href="#" className="navigation__link">О проекте</a></li>
               <li><a href="#" className="navigation__link">Технологии</a></li>
-              <li><a href="#" className="navigation__link">Студент</a></li>
+              <li><a href="#" className="navigation__link">Студентка</a></li>
               </ul>
-              </nav>
+            </div>
             </div>
             <div className="about">
-              <p className="about__title">О проекте</p>
+              <h2 className="about__title">О проекте</h2>
+              <div className="about__line"/>
 
               <div className="columns">
                 <div className="column">
-                  <p>Дипломный проект включал 5 этапов</p>
-                  <p>Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+                  <p className="column__title">Дипломный проект включал 5 этапов</p>
+                  <p className="column__text">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
                 </div>
                 <div className="column">
-                  <p>На выполнение диплома ушло 5 недель</p>
-                  <p>У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+                  <p className="column__title">На выполнение диплома ушло 5 недель</p>
+                  <p className="column__text">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+                </div>
+            </div>
+              <div className="about__scale">
+              <div className="scale__visual scale__visual_green">1 неделя</div>
+              <div className="scale__visual scale__visual_grey">4 недели</div>
+              </div>
+              <div className="about__scale scale__text">
+              <div className="scale__visual">Back-end</div>
+              <div className="scale__visual">Front-end</div>
+              </div>
+              
+            </div>
+            <div className="techs">
+            <h2 className="about__title">Технологии</h2>
+            <div className="about__line"/>
+            <h3 className="techs__title">7 технологий</h3>
+            <p className="techs__text">На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
+            <ul className="navigation__menu">
+            <li className="navigation__link navigation__link_square">HTML</li>
+            <li className="navigation__link navigation__link_square">CSS</li>
+            <li className="navigation__link navigation__link_square">JS</li>
+            <li className="navigation__link navigation__link_square">React</li>
+            <li className="navigation__link navigation__link_square">Git</li>
+            <li className="navigation__link navigation__link_square">Express.js</li>
+            <li className="navigation__link navigation__link_square">mongoDB</li>
+            </ul>
+            </div>
+            <div className="aboutMe">
+            <h2 className="about__title">Студентка</h2>
+            <div className="about__line"/>
+
+            
+              <div className="aboutMe__info">
+              <div className="aboutMe__text">
+                <p className="aboutMe__name">Елизавета</p>
+                <p className="aboutMe__profession">Фронтенд-разработчик, 30 лет</p>
+                <p className="aboutMe__description">L25</p>
+                <div className="aboutMe__social">
+                <a href="https://www.facebook.com/lizaveta.silent/" className="">Facebook</a>
+                <a href="https://github.com/lizasilent" className="">Github</a>
                 </div>
               </div>
-              <div>1 неделя</div>
-              <p>Back-end</p>
-              <div>4 недели</div>
-              <p>Front-end</p>
-              
+              <img className="aboutMe__avatar"></img>
+              </div>
+              </div>
 
-            </div>
-            <div className="techs"></div>
-            <div className="aboutMe"></div>
             <div className="portfolio"></div>
           </main>
           
