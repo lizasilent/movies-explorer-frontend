a23374975a032aea28236219bfd68183ab205992
Liza_silent <elizavetad@ya.ru>
2021-04-20 17:27:31 +0300

    еще один на подходе

--- src/App.js
+++ src/App.js
@@ -16,17 +16,35 @@ function App() {
           </header>
           <main>
             <div className="promo">
-              <p className="promo__title">Учебный проект студента факультета Веб-разработки.</p>
+              <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
             </div>
             <div className="navigation">
               <nav>
-              <a href="#" className="navigation__link">О проекте</a>
-              <a href="#" className="navigation__link">Технологии</a>
-              <a href="#" className="navigation__link">Студент</a>
-                </nav>
+                <ul className="navigation__menu">
+              <li><a href="#" className="navigation__link">О проекте</a></li>
+              <li><a href="#" className="navigation__link">Технологии</a></li>
+              <li><a href="#" className="navigation__link">Студент</a></li>
+              </ul>
+              </nav>
             </div>
             <div className="about">
-              <p className="">О проекте</p>
+              <p className="about__title">О проекте</p>
+
+              <div className="columns">
+                <div className="column">
+                  <p>Дипломный проект включал 5 этапов</p>
+                  <p>Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+                </div>
+                <div className="column">
+                  <p>На выполнение диплома ушло 5 недель</p>
+                  <p>У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+                </div>
+              </div>
+              <div>1 неделя</div>
+              <p>Back-end</p>
+              <div>4 недели</div>
+              <p>Front-end</p>
+              
 
             </div>
             <div className="techs"></div>
