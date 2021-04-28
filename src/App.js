8488b4021941c47caecfe6afb92eb9fc94ae5b41
Liza_silent <elizavetad@ya.ru>
2021-04-28 20:47:04 +0300

    смена классиков

--- src/App.js
+++ src/App.js
@@ -69,23 +69,23 @@ function App() {
             <li className="navigation__link_square">mongoDB</li>
             </ul>
             </div>
-            <div className="aboutMe">
+            <div className="about-me">
             <h2 className="about__title">Студентка</h2>
             <div className="about__line"/>
 
 
-              <div className="aboutMe__info">
-                <div className="aboutMe__text">
-                  <p className="aboutMe__name">Елизавета</p>
-                  <p className="aboutMe__profession">Фронтенд-разработчица, 30 лет</p>
-                  <p className="aboutMe__description">Живу в Петербурге, когда-то закончила филфак, а теперь руковожу книжным интернет-магазином. Очень люблю все что связано с наукой, литературой, музыкой, современной культурой. В Практикум сначала пошла чтобы глубже понять собственный сайт, но обучение оказалось таким интересным и сложным, что, надеюсь, я смогу применить это всё и в работе.
+              <div className="about-me__info">
+                <div className="about-me__text">
+                  <p className="about-me__name">Елизавета</p>
+                  <p className="about-me__profession">Фронтенд-разработчица, 30 лет</p>
+                  <p className="about-me__description">Живу в Петербурге, когда-то закончила филфак, а теперь руковожу книжным интернет-магазином. Очень люблю все что связано с наукой, литературой, музыкой, современной культурой. В Практикум сначала пошла чтобы глубже понять собственный сайт, но обучение оказалось таким интересным и сложным, что, надеюсь, я смогу применить это всё и в работе.
                   </p>
-                  <div className="aboutMe__social">
-                  <a href="https://www.facebook.com/lizaveta.silent/" className="aboutMe__link">Facebook</a>
-                  <a href="https://github.com/lizasilent" className="aboutMe__link">Github</a>
+                  <div className="about-me__social">
+                  <a href="https://www.facebook.com/lizaveta.silent/" className="about-me__link">Facebook</a>
+                  <a href="https://github.com/lizasilent" className="about-me__link">Github</a>
                   </div>
                 </div>
-                <img className="aboutMe__avatar" src={myPhoto} alt="аватар"></img>
+                <img className="about-me__avatar" src={myPhoto} alt="аватар"></img>
                 </div>
 
 
