82245de9bad6dd955d10f51ba5ac976098c44c9b
lizasilent <elizavetad@yandex.ru>
2021-04-28 23:39:19 +0300

    хедер и футер в компонентах

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Footer.css'
+
+function Footer() {
+  return(
+    <footer className="footer">
+      <div className="footer__title">
+        Учебный проект Яндекс.Практикум х BeatFilm.
+      </div>
+      <div className="portfolio__line" />
+      <div className="footer__infobox">
+        <p className="footer__copyright">© 2020</p>
+        <ul className="footer__links">
+          <li>
+            <a href="https://praktikum.yandex.ru/" className="footer__link">
+              Яндекс.Практикум
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/lizasilent"
+              className="footer__link"
+            >
+              Github
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.facebook.com/lizaveta.silent/"
+              className="footer__link"
+            >
+              Facebook
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  )
+
+}
+
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,51 @@
+/* eslint-disable no-lone-blocks */
+import React from 'react';
+import './Header.css' // импортируем CSS-файл
+import mainLogo from "../../images/logo.png";
+// import userLogo from "../../images/main_icon.png";
+
+
+function Header() {
+  return(
+
+    <header className="header">
+      <div className="header__logo">
+        <img alt="лого" src={mainLogo} />
+      </div>
+      <div className="header__container">
+        <a href="#" className="header__signup">
+          Регистрация
+        </a>
+        <button className="header__button">Войти</button>
+      </div>
+    </header>
+
+
+  )
+}
+
+
+export default Header;
+
+
+
+{/* <header className="header header_white">
+<div className="header__logo">
+  <img alt="лого" src={mainLogo} />
+</div>
+<div>
+  {/* <div className="header__burger-menu">
+      <span></span>
+  </div> */}
+//   <div className="header__container">
+//     <a className="header__signup">Фильмы</a>
+//     <a className="header__signup">Сохраненные фильмы</a>
+//   </div>
+// </div>
+// <div className="header__container">
+//   <a className="header__signup">Аккаунт</a>
+//   <a href="" className="header__signup">
+//     <img src={userLogo} />
+//   </a>
+// </div>
+// </header> */}
