556aafd97ff9fa39021f689fb7ae9d1555c0f125
Liza_silent <elizavetad@ya.ru>
2021-05-04 19:06:42 +0300

    revew fix 4

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -7,21 +7,21 @@ function Portfolio() {
       <p className="portfolio__title">Портфолио</p>
       <div className="portfolio__menu">
         <p className="portfolio__text">Статичный сайт</p>
-        <a href="#" className="portfolio__link">
+        <a href="https://github.com/lizasilent/how-to-learn" target="blank" className="portfolio__link">
           ↗
         </a>
       </div>
       <div className="portfolio__line" />
       <div className="portfolio__menu">
         <p className="portfolio__text">Адаптивный сайт</p>
-        <a href="#" className="portfolio__link">
+        <a href="https://github.com/lizasilent/mesto_functional" target="blank" className="portfolio__link">
           ↗
         </a>
       </div>
       <div className="portfolio__line" />
       <div className="portfolio__menu">
         <p className="portfolio__text">Одностраничное приложение</p>
-        <a href="#" className="portfolio__link">
+        <a href="https://github.com/lizasilent/russian-travel" target="blank" className="portfolio__link">
           ↗
         </a>
       </div>
